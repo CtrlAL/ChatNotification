e5a647a702b8c5fa8dd09d61a5da413f96fdf470
unknown <sasha.lartsev@gmail.com>
2025-09-23 01:27:17 +0400

    Fix settinmgs

--- Kafka/Configs/KafkaSettings.cs
+++ Kafka/Configs/KafkaSettings.cs
@@ -4,6 +4,6 @@
     {
         public string BootstrapServers { get; set; }
         public string Topic { get; set; }
-        public string GruopId {  get; set; }
+        public string GroupId {  get; set; }
     }
 }
