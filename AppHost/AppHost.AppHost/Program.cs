2b45a5fba49719f92147df3471bc434b5b65a9a6
unknown <sasha.lartsev@gmail.com>
2025-09-25 22:27:47 +0400

    Add container register from .net aspire

--- AppHost/AppHost.AppHost/Program.cs
+++ AppHost/AppHost.AppHost/Program.cs
@@ -5,4 +5,20 @@ builder.AddProject<Projects.TelegramService>("TelegramService");
 builder.AddProject<Projects.NotificationService>("Notification");
 builder.AddProject<Projects.RegistrationService>("registrationservice");
 
+var mongo = builder.AddMongoDB("mongodb")
+                   .WithEnvironment("MONGO_INITDB_ROOT_USERNAME", "admin")
+                   .WithEnvironment("MONGO_INITDB_ROOT_PASSWORD", "123");
+
+var redis = builder.AddRedis("redis");
+
+var kafka = builder.AddContainer("kafka", "apache/kafka:latest")
+                   .WithEndpoint(port: 9092, targetPort: 9092)
+                   .WithEndpoint(port: 9093, targetPort: 9093);
+
+var keycloak = builder.AddContainer("keycloak", "quay.io/keycloak/keycloak:24.0")
+                      .WithHttpEndpoint(port: 8090, targetPort: 8080)
+                      .WithEnvironment("KEYCLOAK_ADMIN", "admin")
+                      .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
+                      .WithArgs("start-dev");
+
 builder.Build().Run();
