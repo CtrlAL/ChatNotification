29cf571697c5eb3cfd85b4153c2c0fab0dce925c
unknown <sasha.lartsev@gmail.com>
2025-09-25 22:51:46 +0400

    Add Aspire regster services

--- AppHost/AppHost.AppHost/Program.cs
+++ AppHost/AppHost.AppHost/Program.cs
@@ -1,24 +1,32 @@
+﻿using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.ChatService>("Chat");
-builder.AddProject<Projects.TelegramService>("TelegramService");
-builder.AddProject<Projects.NotificationService>("Notification");
-builder.AddProject<Projects.RegistrationService>("registrationservice");
-
-var mongo = builder.AddMongoDB("mongodb")
+var mongo = builder.AddMongoDB("mongodb-auth")
                    .WithEnvironment("MONGO_INITDB_ROOT_USERNAME", "admin")
                    .WithEnvironment("MONGO_INITDB_ROOT_PASSWORD", "123");
 
 var redis = builder.AddRedis("redis");
 
-var kafka = builder.AddContainer("kafka", "apache/kafka:latest")
-                   .WithEndpoint(port: 9092, targetPort: 9092)
-                   .WithEndpoint(port: 9093, targetPort: 9093);
+var kafka = builder.AddKafka("kafka");
 
-var keycloak = builder.AddContainer("keycloak", "quay.io/keycloak/keycloak:24.0")
-                      .WithHttpEndpoint(port: 8090, targetPort: 8080)
-                      .WithEnvironment("KEYCLOAK_ADMIN", "admin")
-                      .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
-                      .WithArgs("start-dev");
+var keycloak = builder.AddKeycloakContainer("keycloak").AddRealm("dev");
+
+builder.AddProject<Projects.ChatService>("Chat")
+       .WithReference(mongo)
+       .WithReference(redis)
+       .WithReference(kafka)
+       .WithReference(keycloak);
+
+builder.AddProject<Projects.TelegramService>("TelegramService")
+       .WithReference(redis)
+       .WithReference(kafka);
+
+builder.AddProject<Projects.NotificationService>("Notification")
+       .WithReference(kafka);
+
+builder.AddProject<Projects.RegistrationService>("RegistrationService")
+       .WithReference(mongo)
+       .WithReference(keycloak);
 
 builder.Build().Run();
