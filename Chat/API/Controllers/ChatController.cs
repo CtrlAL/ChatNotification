400030255e5ca9be008804e7873eb6531fbc0824
unknown <sasha.lartsev@gmail.com>
2025-09-25 20:05:28 +0400

    Chat list

--- Chat/API/Controllers/ChatController.cs
+++ Chat/API/Controllers/ChatController.cs
@@ -5,7 +5,7 @@ using ChatService.API.Infrastructure.Models.Get;
 using ChatService.API.Infrastructure.Models.Get.Response;
 using ChatService.Domain;
 using System.Security.Claims;
-using System;
+using ChatService.Domain.Filters;
 
 namespace ChatService.API.Controllers
 {
@@ -47,14 +47,45 @@ namespace ChatService.API.Controllers
             return Ok(new CreateResponse(result));
         }
 
-        [HttpGet("message-list")]
-        public async Task<ActionResult<SearchResultResponse<GetChatMessageModel>>> GetMessageList()
+        [HttpGet("message-list/{chatId}")]
+        public async Task<ActionResult<SearchResultResponse<GetChatMessageModel>>> GetMessageList([FromRoute] string chatId)
         {
+            var chat = await _messageRepository.GetAsync(chatId);
+
+            var authResult = await _authorizationService.AuthorizeAsync(User, chat, "ChatOwner");
+
+            if (!authResult.Succeeded)
+            {
+                return User.Identity?.IsAuthenticated == true ? Forbid() : Challenge();
+            }
+
             var result = (await _messageRepository.GetAsync(filter: new())).Select(GetChatMessageModel.ToModel).ToList();
             var searchResult = new SearchResultResponse<GetChatMessageModel>(result ?? new List<GetChatMessageModel>());
 
             return Ok(searchResult);
         }
+
+        [HttpPost("chat-list")]
+        public async Task<ActionResult<SearchResultResponse<GetChatModel>>> GetChatList()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null)
+            {
+                return Forbid();
+            }
+
+            var filter = new UserResourseFilter
+            {
+                UserId = userId
+            };
+
+            var result = (await _chatRepository.GetAsync(filter)).Select(GetChatModel.ToModel).ToList();
+            var searchResult = new SearchResultResponse<GetChatModel>(result ?? new List<GetChatModel>());
+
+            return Ok(searchResult);
+        }
+
 
         [HttpGet("{id}")]
         public async Task<ActionResult<SearchResultResponse<GetChatMessageModel>>> GetChat([FromRoute] string id)
@@ -71,13 +102,21 @@ namespace ChatService.API.Controllers
             return Ok(GetChatMessageModel.ToModel(result));
         }
 
-        [HttpPost("chat-list")]
-        public async Task<ActionResult<SearchResultResponse<GetChatModel>>> GetChatList()
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SearchResultResponse<GetChatMessageModel>>> Delete([FromRoute] string id)
         {
-            var result = (await _chatRepository.GetAsync(filter: new())).Select(GetChatModel.ToModel).ToList();
-            var searchResult = new SearchResultResponse<GetChatModel>(result ?? new List<GetChatModel>());
+            var result = await _messageRepository.GetAsync(id);
 
-            return Ok(searchResult);
+            var authResult = await _authorizationService.AuthorizeAsync(User, result, "ChatOwner");
+
+            if (!authResult.Succeeded)
+            {
+                return User.Identity?.IsAuthenticated == true ? Forbid() : Challenge();
+            }
+
+            await _messageRepository.RemoveAsync(id);
+
+            return Ok();
         }
     }
 }
